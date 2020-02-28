863578ac6ed170a56632a22b42343b4f6ced2d0d
Samjenglish <samenglish@cmail.carleton.ca>
2020-02-28 18:04:32 -0500

    Initial files for Spring Boot application, connected to PostgreSQL DB

---
+++ src/main/java/quickndirty/minisurveymonkey/MinisurveymonkeyApplication.java
@@ -0,0 +1,13 @@
+package quickndirty.minisurveymonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisurveymonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisurveymonkeyApplication.class, args);
+	}
+
+}
