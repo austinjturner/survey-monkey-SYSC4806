df712068262e0eb7875c4fd50f9f9425148a7fd8
Austin Turner <32046329+austinjturner@users.noreply.github.com>
2020-03-02 16:55:47 -0500

    Update repository annotations to use RepositoryRestResource

--- src/main/java/quickndirty/minisurveymonkey/ResponseRepository.java
+++ src/main/java/quickndirty/minisurveymonkey/ResponseRepository.java
@@ -1,9 +1,10 @@
 package quickndirty.minisurveymonkey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel="response", path="response")
 public interface ResponseRepository extends JpaRepository<Response, Integer> {
 
 
