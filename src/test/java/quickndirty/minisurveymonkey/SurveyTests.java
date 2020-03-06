314c0a297755fbaec0a87c7feea6e4f9bbc22df6
Aleksandar1231 <aleksandarsavic@cmail.carleton.ca>
2020-03-05 23:26:31 -0500

    survey tests

--- src/test/java/quickndirty/minisurveymonkey/SurveyTests.java
+++ src/test/java/quickndirty/minisurveymonkey/SurveyTests.java
@@ -2,13 +2,58 @@ package quickndirty.minisurveymonkey;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SurveyTests {
-    Survey survey = new Survey();
+    Survey survey = new Survey("SurveyTest");
+    List<Question> questions = new ArrayList<>();
+    Question question1 = new TextQuestion();
+    Question question2 = new TextQuestion();
 
     @Test
     public void isEmpty(){
-        assertEquals(0, survey.getQuestions().size());
+        assertEquals(0, survey.size());
     }
+
+    @Test
+    public void isClosed(){
+        assertFalse(survey.isClosed());
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("SurveyTest", survey.getName());
+    }
+
+    @Test
+    public void addQuestion(){
+        survey.addQuestion(question1);
+        assertEquals(survey.getQuestions().get(0), question1);
+    }
+
+    @Test
+    public void removeQuestion(){
+        survey.removeQuestion(question1);
+        assertEquals(0, survey.size());
+    }
+
+    @Test
+    public void setQuestions(){
+        questions.add(question1);
+        questions.add(question2);
+        survey.setQuestions(questions);
+        assertEquals(questions, survey.getQuestions());
+    }
+
+    @Test
+    public void close(){
+        survey.close();
+        assertTrue(survey.isClosed());
+    }
+
+
+
 }
