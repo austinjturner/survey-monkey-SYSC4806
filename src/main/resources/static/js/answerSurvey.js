6a07b34dbb9bc2ff0b5357e3222582be9994df40
Nikola Erak <nikolaerak5@gmail.com>
2020-03-14 14:21:35 -0400

    improve ui

--- src/main/resources/static/js/answerSurvey.js
+++ src/main/resources/static/js/answerSurvey.js
@@ -4,6 +4,10 @@ $(document).ready(() => {
     const nextQuestionLink = $(`#nextBtnLink`).attr(`href`);
     $(`#nextBtn`).remove();
 
+    getQuestion(parseInt($('#surveyQuestionValue')[0].value.split('question/')[1])).then(function(result){
+        loadAnsweringDiv(result);
+    })
+
     $("#submitBtn").click(function(event) {
         event.preventDefault();
         const form = $('#answerSurveyForm');
@@ -30,3 +34,49 @@ $(document).ready(() => {
         })
     });
 });
+
+function loadAnsweringDiv(question) {
+
+    var questionHTML = '';
+
+    switch ($('#surveyTypeValue')[0].value) {
+        case "TEXT" :
+            questionHTML = '<div class="input-group-prepend">\n' +
+                '           <span class="input-group-text" ' + question.getPrompt()+ '></span>\n' +
+                '           </div>\n' +
+                '           <textarea class="form-control" id="surveyTextArea" rows="3" name="answer"></textarea>'
+            break;
+        case "MC" :
+            //UPDATE
+            questionHTML = '<div class="input-group-prepend">\n' +
+                '           <span class="input-group-text" th:text="${questions.get(questionNumber).getPrompt()}"></span>\n' +
+                '           </div>\n' +
+                '           <textarea class="form-control" id="surveyTextArea" rows="3" name="answer"></textarea>'
+            break;
+        case "RANGE" :
+            //UPDATE
+            questionHTML = '<div class="input-group-prepend">\n' +
+                '           <span class="input-group-text" th:text="${questions.get(questionNumber).getPrompt()}"></span>\n' +
+                '           </div>\n' +
+                '           <textarea class="form-control" id="surveyTextArea" rows="3" name="answer"></textarea>'
+            break;
+    }
+
+    $('#questionContent').append(questionHTML);
+}
+
+function getQuestion(id) {
+    return new Promise((resolve, reject) => {
+        apiUrl = window.location.origin + '/api/question/' + id
+        $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            success: function(data) {
+                resolve(data)
+            },
+            error: function(error) {
+                reject(error)
+            },
+        })
+    })
+}
